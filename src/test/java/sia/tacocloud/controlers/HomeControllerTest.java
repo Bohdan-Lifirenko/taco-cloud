138d8c7f6f8785b17136b1c6938a2f6e571ddf17
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-08-13 17:39:23 +0300

    Create part of taco web app, where you can create our own taco and then do the order.

---
+++ src/test/java/sia/tacocloud/controlers/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package sia.tacocloud.controlers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test_home_page() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(
+                Matchers.containsString("Welcome to...")
+            ));
+
+    }
+}
